eb0e3185e40017ab43193766de2e66d8ee167d8c
fzTelU <fazaalexander@student.telkomuniversity.ac.id>
2022-03-26 09:37:51 +0700

    Add project files.

---
+++ modul5_1302204062/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace modul5_1302204062
+     class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
